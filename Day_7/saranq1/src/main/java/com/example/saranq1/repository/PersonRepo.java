6dcac9f097399e2fa4a607d7352d43158304398a
Badhmasharan <124858439+Badhmasharan@users.noreply.github.com>
2024-04-29 15:32:53 +0530

    Add files via upload

---
+++ Day_7/saranq1/src/main/java/com/example/saranq1/repository/PersonRepo.java
@@ -0,0 +1,15 @@
+package com.example.saranq1.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.saranq1.model.Person;
+
+public interface PersonRepo extends JpaRepository<Person,Integer> {
+
+    @Query("SELECT s FROM Person s where s.age > :Age")
+    public List<Person> findByStudentAge(int Age);
+
+}
