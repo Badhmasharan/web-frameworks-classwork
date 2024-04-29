a9ca9f83999b5d364e67eb51aa0b9dfcbac83d93
Badhmasharan <124858439+Badhmasharan@users.noreply.github.com>
2024-04-29 14:38:58 +0530

    Add files via upload

---
+++ Day_6/saranq1/src/main/java/com/example/saranq1/Saranq1Application.java
@@ -0,0 +1,13 @@
+package com.example.saranq1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saranq1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Saranq1Application.class, args);
+	}
+
+}
